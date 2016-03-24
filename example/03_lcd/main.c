6edda41c30e65180eb10beb3482b1256bed51f98
ChalieH <horang17502@gmail.com>
2016-03-24 23:39:48 +0900

    lcd example command & data function bug fixed

--- example/03_lcd/main.c
+++ example/03_lcd/main.c
@@ -19,6 +19,7 @@
 #define GPFSEL4         (0x10 / 4)  // 40 ~ 49
 #define GPFSEL5         (0x14 / 4)  // 50 ~ 54
 
+/* define for gpio function select */
 #define PIN0            0
 #define PIN1            3
 #define PIN2            6
@@ -48,9 +49,18 @@
 #define D6              25
 #define D7              26
 
+/* LCD instruction */
+#define LCD_CLR		0x01
+#define	LCD_HOME	0x02
+#define LCD_ENT		0x06
+#define LCD_DSP		0x0F
+#define	LCD_CUR		0x14
+#define LCD_FUNC	0x38
+
 void lcd_init(void);
 void lcd_on(void);
 void lcd_clr(void);
+unsigned char lcd_busy_check();
 void lcd_cmd(unsigned char);
 void lcd_data(unsigned char);
 void lcd_close();
@@ -58,6 +68,7 @@ void lcd_close();
 void msleep(unsigned int millis);
 void usleep(unsigned int micros);
 
+/* global */
 volatile unsigned int *gpio;
 int fd;
 
@@ -73,6 +84,8 @@ int main()
 	lcd_data('A');
 	lcd_data('A');
 
+	while(1);
+
 	lcd_close();
 
     return 0;
@@ -84,9 +97,15 @@ void lcd_close()
 	close(fd);
 }
 
+/*
+ * Function : LCD initialize
+ * Description
+ * 1. GPIO mapping
+ * 2. GPIO output setting (Function select)
+ * 3. LCD initialize command send
+ */
 void lcd_init()
 {
-	printf("lcd_init() start\n");
     //DB0~7 : gpio 19,20,21,22,23,24,25,26
     //RS    : gpio 18
     //RW    : gpio 17
@@ -108,90 +127,100 @@ void lcd_init()
 
     gpio = (volatile unsigned int *)gpio_base_map;
 	
-	printf("lcd_init() gpio addr [%08X]\n", gpio);
-
     /* function select for gpio output */
     gpio[GPFSEL1] = (1 << PIN6) | (1 << PIN7) | (1 << PIN8) | (1 << PIN9);
     gpio[GPFSEL2] = (1 << PIN0) | (1 << PIN1) | (1 << PIN2) | (1 << PIN3) |
                     (1 << PIN4) | (1 << PIN5) | (1 << PIN6);
 
-	printf("GPFSEL1 [%08X]\n", gpio[GPFSEL1]);
-	printf("GPFSEL2 [%08X]\n", gpio[GPFSEL2]);
-
     lcd_on();
     lcd_clr();
-	printf("lcd_init() end\n");
-}
 
-#define LCD_CLR		0x01
-#define	LCD_HOME	0x02
-#define LCD_ENT		0x06
-#define LCD_DSP		0x0F
-#define	LCD_CUR		0x14
-#define LCD_FUNC	0x38
+#if 0 //GPIO output test : OK
+    gpio[GPFSEL1] = (1 << PIN6) | (1 << PIN7) | (1 << PIN8) | (1 << PIN9);
+    gpio[GPFSEL2] = (1 << PIN0) | (1 << PIN1) | (1 << PIN2) | (1 << PIN3) |
+                    (1 << PIN4) | (1 << PIN5) | (1 << PIN6);
+	while(1)
+	{
+    	gpio[GPSET0] |= (1 << EN) | (1 << RW) | (1 << RS) | (0xFF << D0);
+		msleep(1000);
+    	gpio[GPCLR0] |= (1 << EN) | (1 << RW) | (1 << RS) | (0xFF << D0);
+		msleep(1000);
+	}
+#endif
+}
 
 void lcd_on()
 {
 	printf("lcd_on() start\n");
-#if 1
+#if 0 /* wowcat src */
 	lcd_cmd(LCD_FUNC);
 	lcd_cmd(LCD_DSP);
 	lcd_cmd(LCD_ENT);
 	lcd_cmd(LCD_CUR);
 	lcd_cmd(LCD_CLR);
 	lcd_cmd(LCD_HOME);
+	msleep(2);
 #else
     lcd_cmd(0x38);
     lcd_cmd(0x0C);
     lcd_cmd(0x01);
     lcd_cmd(0x06);
     lcd_cmd(0x01);
+	msleep(2);
 #endif
-	printf("lcd_on() end\n");
 }
 
 void lcd_clr()
 {
 	printf("lcd_clr() start\n");
-    lcd_cmd(1);
-    msleep(10);
-    lcd_cmd(2);
-    msleep(10);
-	printf("lcd_clr() end\n");
+    lcd_cmd(LCD_CLR);
+    msleep(1);
+    lcd_cmd(LCD_HOME);
+    msleep(1);
+}
+
+unsigned char lcd_busy_check()
+{
+	usleep(200);
+	usleep(200);
+	usleep(200);
+	usleep(200);
+	usleep(200);
+	usleep(200);
+	usleep(200);
+
+	return 0;
 }
 
 void lcd_cmd(unsigned char data)
 {
 	printf("lcd_cmd() start %02X\n", data);
+	while(lcd_busy_check()) {};
+
     gpio[GPCLR0] |= (1 << EN) | (1 << RW) | (1 << RS);
+	usleep(2);
     gpio[GPSET0] |= (data << D0);
-    msleep(10);
     usleep(50);
-    gpio[GPSET0] |= (data << EN);
-    msleep(10);
+    gpio[GPSET0] |= (1 << EN);
     usleep(20);
-    gpio[GPCLR0] |= (data << EN);
-    msleep(10);
+    gpio[GPCLR0] |= (1 << EN);
     usleep(50);
-	printf("lcd_cmd() end\n");
 }
 
 void lcd_data(unsigned char data)
 {
     //check delay
 	printf("lcd_data() start %02X\n", data);
+	while(lcd_busy_check()) {};
     gpio[GPCLR0] |= (1 << EN) | (1 << RW);
     gpio[GPSET0] |= (1 << RS);
+	usleep(2);
     gpio[GPSET0] |= (data << D0);
-    msleep(10);
     usleep(50);
-    gpio[GPSET0] |= (data << EN);
-    msleep(10);
+    gpio[GPSET0] |= (1 << EN);
     usleep(20);
-    gpio[GPCLR0] |= (data << EN);
-    msleep(10);
+    gpio[GPCLR0] |= (1 << EN);
     usleep(50);
-	printf("lcd_data() end\n");
 }
 
 void msleep(unsigned int millis)
